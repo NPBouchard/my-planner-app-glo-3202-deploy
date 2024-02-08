58a753840cbd48f9f23be0a639828299d4d8add7
NPBouchard <97470345+NPBouchard@users.noreply.github.com>
2024-02-08 12:55:08 -0500

    tbk

--- app/script/EventList.tsx
+++ app/script/EventList.tsx
@@ -4,12 +4,13 @@ export default async function EventList(): Promise<JSX.Element> {
     const userID = 1; 
 
     const { rows } = await sql`SELECT * FROM events WHERE Userid = ${userID}`;
-    console.log(rows);
+    
+
   return (
     <div className="container mx-auto px-4 py-8">
       <h2 className="text-2xl font-bold mb-4">Liste des Événements</h2>
       <div className="space-y-4">
-        {rows.map((event, index) => (
+        {rows.map((event: any, index: any) => (
           <div key={index} className="bg-white shadow overflow-hidden rounded-md px-6 py-4">
             <div className="pb-4">
               <h3 className="text-xl font-semibold">{event.eventname}</h3>
