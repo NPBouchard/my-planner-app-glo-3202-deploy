de3f7fd886c8e7eaf31e444eabec9d59a0e5d98f
NPBouchard <97470345+NPBouchard@users.noreply.github.com>
2024-02-08 12:16:00 -0500

    corrected bug in EventList

--- app/script/EventList.tsx
+++ app/script/EventList.tsx
@@ -3,7 +3,7 @@ import { sql } from "@vercel/postgres";
 export default async function EventList(): Promise<JSX.Element> {
     const userID = 1; 
 
-    const { rows } = await sql`SELECT * FROM events WHERE UserID = ${userID}`;
+    const { rows } = await sql`SELECT * FROM events WHERE UserId = ${userID}`;
 
   return (
     <div className="container mx-auto px-4 py-8">
@@ -12,13 +12,13 @@ export default async function EventList(): Promise<JSX.Element> {
         {rows.map((event: any, index : any) => (
           <div key={index} className="bg-white shadow overflow-hidden rounded-md px-6 py-4">
             <div className="pb-4">
-              <h3 className="text-xl font-semibold">{event.eventName}</h3>
-              <p className="text-sm text-gray-500">Organisé par: UserID {event.userId}</p>
-              <p className="text-sm text-gray-500">Date: {event.eventDate}</p>
-              <p className="text-sm text-gray-500">Lieu: {event.eventLocation}</p>
+              <h3 className="text-xl font-semibold">{event.Eventname}</h3>
+              <p className="text-sm text-gray-500">Organisé par: UserID {event.Userid}</p>
+              <p className="text-sm text-gray-500">Date: {event.Eventdate}</p>
+              <p className="text-sm text-gray-500">Lieu: {event.Eventlocation}</p>
             </div>
             <div>
-              <p className="text-gray-700">{event.eventDescription}</p>
+              <p className="text-gray-700">{event.Eventdescription}</p>
             </div>
           </div>
         ))}
