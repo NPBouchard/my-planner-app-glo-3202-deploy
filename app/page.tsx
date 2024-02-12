24cfa008aec94a721d6b9df592172d5b8ab53c0e
NPBouchard <97470345+NPBouchard@users.noreply.github.com>
2024-02-12 15:32:17 -0500

    added api call to eventlist

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Format from "./components/Format";
 import React from "react";
 import Link from "next/link";
--- app/script/EventList.tsx
+++ app/script/EventList.tsx
@@ -1,8 +1,35 @@
+"use client"
+import React, {useState} from "react";
+
+
+
 export default async function EventList(): Promise<JSX.Element> {
     const userID = 1; 
+    const [events, setEvents] = useState(null)
+
+    const loadEvents = async () => {
+        try {
+            const res = await fetch(`https://my-planner-app-glo-3202-deploy.vercel.app/api/events/${userID}`, {
+              method: 'GET',
+              headers: {
+                'content-type': 'application/json'
+              }
+            })
+            console.log(res)
+            if(res.ok){
+              console.log("Yeai!")
+            }else{
+              console.log("Oops! Something is wrong.")
+            }
+          } catch (error) {
+              console.log(error)
+          }
+      }
+    
 
   return (
     <div className="container mx-auto px-4 py-8">
+        <button onClick={loadEvents}>Click</button>
   <h2 className="text-2xl font-bold mb-4">Liste des Événements</h2>
   <div className="grid grid-cols-5 gap-4">
     {/* {rows.map((event: any, index: any) => (
