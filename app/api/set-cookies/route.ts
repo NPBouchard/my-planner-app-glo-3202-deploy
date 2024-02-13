43d833a6f42a5c4e7c18a17c3542f958d961be3e
NPBouchard <97470345+NPBouchard@users.noreply.github.com>
2024-02-13 17:35:03 -0500

    del

--- app/api/set-cookies/route.ts
+++
@@ -1,8 +0,0 @@
-// import {NextRequest, NextResponse} from "next/server";
-// import Cookies from 'cookies';
-
-// export default (req: NextRequest, res: NextResponse) => {
-//   const cookies = new Cookies(req, res);
-//   cookies.set('myCookie', 'value', { httpOnly: true, sameSite: 'lax' });
-//   res.status(200).json({ message: 'Cookie set' });
-// };
