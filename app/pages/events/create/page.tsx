b78feaa04c6d90d9c6627f979ed14a90dd496a07
NPBouchard <97470345+NPBouchard@users.noreply.github.com>
2024-02-13 17:06:50 -0500

    modified + added cookies

--- app/pages/events/create/page.tsx
+++ app/pages/events/create/page.tsx
@@ -85,7 +85,7 @@ const CreateEventPage: React.FC = () => {
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full px-3">
               <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="location">
-                Description
+                Location
               </label>
               <textarea className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white h-48 resize-none" id="location" placeholder="La location de l'événement" name="location" value={event.location} onChange={handleChange}></textarea>
             </div>
