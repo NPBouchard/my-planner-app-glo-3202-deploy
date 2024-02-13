99aa915e78db92db9c8853b75c21891b42800e65
NPBouchard <97470345+NPBouchard@users.noreply.github.com>
2024-02-13 17:39:01 -0500

    added btn

--- app/pages/events/create/page.tsx
+++ app/pages/events/create/page.tsx
@@ -103,6 +103,9 @@ const CreateEventPage: React.FC = () => {
               <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">
                 Créer Événement
               </button>
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Location Actuelle
+              </button>
             </div>
           </div>
         </form>
