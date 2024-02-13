275fe683373d7b874776c261f4d7f6c40ed2dd91
NPBouchard <97470345+NPBouchard@users.noreply.github.com>
2024-02-13 18:23:32 -0500

    change

--- app/pages/events/create/page.tsx
+++ app/pages/events/create/page.tsx
@@ -74,7 +74,9 @@ const CreateEventPage: React.FC = () => {
   return (
     <Format>
       <div className="max-w-4xl mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold text-center mb-6">Créer un Nouvel Événement</h1>
+        <h1 className="text-3xl font-bold text-center mb-6">Créer un Nouvel Événement <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={UseActualLocation}>
+                Location Actuelle
+              </button></h1>
         <form onSubmit={handleSubmit} className="w-full max-w-lg mx-auto">
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full px-3">
@@ -95,9 +97,7 @@ const CreateEventPage: React.FC = () => {
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full px-3">
               <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="location">
-                Location <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={UseActualLocation}>
-                Location Actuelle
-              </button>
+                Location
               </label>
               <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="location" type="text" placeholder="Location de votre événement" name="location" value={event.location} onChange={handleChange} />
             </div>
