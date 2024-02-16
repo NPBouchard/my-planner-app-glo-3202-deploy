5ee7ef8f9a5190625e86f129724cd6fbd0eadbb7
NPBouchard <97470345+NPBouchard@users.noreply.github.com>
2024-02-16 14:33:42 -0500

    modified back

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import { sql } from '@vercel/postgres';
 
 export async function GET() {
-	const { rows } = await sql`SELECT id FROM users`;
+	const { rows } = await sql`SELECT id, username FROM users`;
 
 	const json = {
 		rows,
