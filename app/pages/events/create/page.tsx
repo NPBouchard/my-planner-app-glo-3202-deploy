28174963cb89cb295584c49b2e236e45983b9d50
NPBouchard <97470345+NPBouchard@users.noreply.github.com>
2024-02-16 20:19:16 -0500

    added onclick

--- app/pages/events/create/page.tsx
+++ app/pages/events/create/page.tsx
@@ -138,6 +138,13 @@ const CreateEventPage: React.FC = () => {
 		}
 	};
 
+  const useLocationFromList = (location: string) => {
+    setEvent((prevEvent) => ({
+			...prevEvent,
+			location: location,
+		}));
+  }
+
 	return (
 		<Format>
 			<div className="max-w-4xl mx-auto px-4 py-8">
@@ -259,7 +266,7 @@ const CreateEventPage: React.FC = () => {
 									key={index}
 									className="mb-2"
 								>
-									<div className="flex items-center">
+									<div className="flex items-center" onClick={() => useLocationFromList(location)} >
 										<MapPinIcon className="w-5 h-5 mr-2 text-blue-500" />
 										<span>{location}</span>
 									</div>
